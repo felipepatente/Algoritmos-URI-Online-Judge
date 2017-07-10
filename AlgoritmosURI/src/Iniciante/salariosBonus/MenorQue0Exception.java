8478e5aadd80416d82739e1d08ae9c8157bfaa30
felipepatente <felipepatente@yahoo.com.br>
2017-07-10 14:22:05 -0300

    Calculando salario com bônus

---
+++ AlgoritmosURI/src/Iniciante/salariosBonus/MenorQue0Exception.java
@@ -0,0 +1,9 @@
+package Iniciante.salariosBonus;
+
+public class MenorQue0Exception extends Exception {
+	
+	public String getMessage(){
+		return "Não é permitido número menor ou igual que 0";
+	}
+
+}
