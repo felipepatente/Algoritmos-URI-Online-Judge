95bb3463d3c9a77ef476ecc3b21fd0e2d880c17c
felipepatente <felipepatente@yahoo.com.br>
2017-07-28 16:26:02 -0300

    Organizandos os arquivos

--- AlgoritmosURI/src/Iniciante/excecoes/MenorQue0Exception.java
+++
@@ -1,9 +0,0 @@
-package Iniciante.excecoes;
-
-public class MenorQue0Exception extends Exception{
-
-	public String getMessage(){
-		return "Digite número maiores ou igual a zero";
-	}
-	
-}
--- AlgoritmosURI/src/Iniciante/salarios/MenorZeroException.java
+++
@@ -1,9 +0,0 @@
-package Iniciante.salarios;
-
-public class MenorZeroException  extends Exception {
-
-	public String getMessage(){
-		return "Não é permitido números menores ou igual a 0";
-	}
-	
-}
